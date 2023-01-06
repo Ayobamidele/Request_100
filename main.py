85e886857eadbfbfdd052ae1ab8ab8ce867e50db
Ayobamidele <ayobamideleewetuga@gmail.com>
2023-01-06 02:06:37 +0100

    Fixed the initial error with the port

--- main.py
+++ main.py
@@ -80,4 +80,5 @@ def result():
 
 
 if __name__ == "__main__":
-    app.run(debug=True, port=os.getenv("PORT", default=5000))
+    app.run(debug=True, port=os.getenv("PORT", default=5000),host=os.getenv("HOST", default="0.0.0.0"))
+   
